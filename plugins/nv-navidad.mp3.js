62fbc855ff7a3c4a43fd25502bb763b4424f00f7
jahhid <dzhoonra@bk.ru>
2022-09-15 17:48:51 +0300

    jahhid / Grishanya

--- plugins/nv-navidad.mp3.js
+++ plugins/nv-navidad.mp3.js
@@ -9,6 +9,6 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /ухожу|до завтра|пока|Пока/
+handler.customPrefix = /Ухожу|До завтра|пока|Пока/
 handler.command = new RegExp
 export default handler
