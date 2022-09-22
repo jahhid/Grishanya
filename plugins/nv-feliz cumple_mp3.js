a2ec60f805df9a9b61673ff9e04048ac3346a95f
jahhid <dzhoonra@bk.ru>
2022-09-22 11:38:36 +0400

    jahhid / Grishanya

--- plugins/nv-feliz cumple_mp3.js
+++ plugins/nv-feliz cumple_mp3.js
@@ -3,8 +3,8 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/progulka.mp3'
-conn.sendFile(m.chat, vn, 'progulka.mp3', null, m, true, {
+let vn = './media/super-mario.mp3'
+conn.sendFile(m.chat, vn, 'super-mario.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
