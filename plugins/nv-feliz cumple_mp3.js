fff0bbe20bd4e60ccf2ae56fc6a96d0bdecba401
jahhid <dzhoonra@bk.ru>
2022-09-22 11:34:10 +0400

    jahhid / Grishanya

--- plugins/nv-feliz cumple_mp3.js
+++ plugins/nv-feliz cumple_mp3.js
@@ -3,13 +3,13 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/Feliz cumple.mp3'
-conn.sendFile(m.chat, vn, 'Feliz cumple.mp3', null, m, true, {
+let vn = './media/progulka.mp3'
+conn.sendFile(m.chat, vn, 'progulka.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /feliz cumpleaños|felizcumpleaños|happy birthday/i
+handler.customPrefix = /погуляю|пошли|лс/i
 handler.command = new RegExp
 handler.fail = null
 handler.exp = 100
