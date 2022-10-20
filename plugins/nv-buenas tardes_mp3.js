bf11dc734a721913152bf97fa0a12a3e6dc274e4
jahhid <dzhoonra@bk.ru>
2022-10-20 17:27:26 +0300

    jahhid / Grishanya

--- plugins/nv-buenas tardes_mp3.js
+++ plugins/nv-buenas tardes_mp3.js
@@ -3,12 +3,12 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/boatarde.mp3'
-conn.sendFile(m.chat, vn, 'boatarde.mp3', null, m, true, {
+let vn = './media/zavtrak.mp3'
+conn.sendFile(m.chat, vn, 'zavtrak.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /buenas tardes|Buenas tardes|boatarde|Boatarde/
+handler.customPrefix = /завтрак|обед|ужин|поесть/
 handler.command = new RegExp
 export default handler
